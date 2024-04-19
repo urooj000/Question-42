5eb7b91f83746099f09387a762b48109676ec3d9
urooj22 <160038688+urooj000@users.noreply.github.com>
2024-04-19 22:32:11 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+"use strict";
+// Great Magicians: Start with a copy of your program from Exercise 39. Write a function called make_great() that modifies the array of magicians by adding the phrase the Great to each magician’s name. Call show_magicians() to see that the list has actually been modified.
+const magicians = ["penn jillette", "Criss Angel", "Teller"];
+function showMagicians(magicians) {
+    console.log(magicians.join("\n"));
+}
+function makeGreat(magicians) {
+    return magicians.map((magician) => `${magician} the Great`);
+}
+console.log("Original magicians:");
+showMagicians(magicians);
+const graetMagicians = makeGreat(magicians);
+console.log("\nModified list:");
+showMagicians(graetMagicians);
